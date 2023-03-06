f21f965b28dcc502baf2f6fc751dcbd99ced2158
Guillaume Duran <guillaumeduran2@gmail.com>
2023-03-06 21:07:09 +0100

    app done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,48 @@
 import Logo from "./Logo.js";
 import Search from "./Search.js";
 import Form from "./Form.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 
 const Nav = ({ stays, setStays }) => {
   const [search, setSearch] = useState(false);
   const [location, setLocation] = useState("");
   const [guests, setGuests] = useState("");
+  const [page, setPage] = useState(false);
+
+  useEffect(() => {
+    if (stays.data) {
+      setPage(true);
+      setLocation("");
+      setGuests("");
+    } else {
+      setPage(false);
+    }
+  }, [stays]);
 
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-5xl px-4 py-8 sm:flex sm:items-center sm:justify-between sm:py-8">
-        <Logo />
-        <Form
-          search={search}
-          setSearch={setSearch}
-          stays={stays}
-          setStays={setStays}
-          location={location}
-          setLocation={setLocation}
-          guests={guests}
-          setGuests={setGuests}
-        />
+      <div className="relative mx-auto max-w-5xl px-4 py-8 sm:flex sm:items-center sm:justify-between sm:py-8">
+        <Link to="/">
+          <Logo />
+        </Link>
+        {(!page && (
+          <Form
+            search={search}
+            setSearch={setSearch}
+            stays={stays}
+            setStays={setStays}
+            location={location}
+            setLocation={setLocation}
+            guests={guests}
+            setGuests={setGuests}
+          />
+        )) || (
+          <Link to="/">
+            <ArrowUturnLeftIcon className="absolute top-8 right-4 h-8 w-8 text-windbnb" />
+          </Link>
+        )}
       </div>
       <Search
         search={search}
